338a5e33f60dc465993a00817abaecc3a4938f3a
Oskari Niskanen <oskari.niskanen97@gmail.com>
2026-06-23 19:16:45 +0300

    fix: docker exposed ip and express app listener.

--- server.ts
+++ server.ts
@@ -292,7 +292,7 @@ async function startServer() {
     });
   }
 
-  app.listen(PORT, '127.0.0.1', () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`[samesecret Server] Listening securely on http://localhost:${PORT}`);
   });
 }
